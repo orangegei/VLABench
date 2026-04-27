3b15fb24fbef5acfd42d619208df4d9d08912f01
orangegei <1659227126@qq.com>
2026-04-27 13:46:22 +0800

    完整测试eval_openvla脚本修改

--- evaluate_openvla.sh
+++ evaluate_openvla.sh
@@ -1,25 +1,29 @@
-#bin/bash
+#!/bin/bash
+set -euo pipefail
 
 export MUJOCO_GL=egl
+export PYOPENGL_PLATFORM=egl
+export MUJOCO_EGL_DEVICE_ID=0
 
-tasks_to_eval="select_fruit select_toy select_poker select_painting select_mahjong" 
-eval_tracks=("track_1_in_distribution" "track_2_cross_category" "track_3_common_sense" "track_4_semantic_instruction" "track_6_unseen_texture")
+eval_tracks=(
+    "track_1_in_distribution"
+    "track_2_cross_category"
+    "track_3_common_sense"
+    "track_4_semantic_instruction"
+    "track_6_unseen_texture"
+)
 
-EPISODE=50
+EPISODE=3
 POLICY="openvla"
 MODEL_CKPT="/seu_share/home/wangshuai01/213221090/hf_cache/openvla-lora"
-
 METRICS="success_rate intention_score progress_score"
 
 for eval_track in "${eval_tracks[@]}"; do
     echo "Evaluating on eval track: $eval_track"
     python scripts/evaluate_policy.py \
-        --tasks $tasks_to_eval \
-        --eval-track $eval_track \
-        --n-episode $EPISODE \
-        --policy $POLICY \
-        --model_ckpt $MODEL_CKPT \
-        --visulization \
-        --metrics $METRICS \
-
+        --eval-track "$eval_track" \
+        --n-episode "$EPISODE" \
+        --policy "$POLICY" \
+        --model_ckpt "$MODEL_CKPT" \
+        --metrics $METRICS
 done
